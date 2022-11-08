cd7d25e43efcd5b7ec823367eb0d2c0a6dd38255
Winters <90045264+NoobInDaHause@users.noreply.github.com>
2022-11-08 11:36:12 +0800

    Update giveawaydonation.py

--- giveawaydonation/giveawaydonation.py
+++ giveawaydonation/giveawaydonation.py
@@ -15,7 +15,7 @@ class GiveawayDonation(commands.Cog):
     """
     
     __version__ = "1.0.0"
-    __author__ = "Richard Winters#2808"
+    __author__ = ("Richard Winters#2808")
     
     def __init__(self, bot: Red):
         self.bot = bot
@@ -37,3 +37,13 @@ class GiveawayDonation(commands.Cog):
         embed.set_footer(text=f"{ctx.guild}", icon_url=f"{ctx.guild.icon_url}")
         await ctx.send(embed=embed, content=pingrole, allowed_mentions=discord.AllowedMentions(roles=True))
         await ctx.message.delete()
+        
+    @checks.bot_has_permissions(embed_links=True)
+    @commands.command(name='gdonotest')
+    async def cmd_gdonotest(self, ctx: commands.Context, message: str = None):
+        """
+        Button test command.
+        
+        Test buttons on message
+        """
+        await ctx.send(type=InteractionType.ChannelMessageWithSource, content="Congrats user you won nothing.", components=[Button(style=ButtonStyle.URL, label="Example Invite Button", url="https://google.com"), Button(style=ButtonStyle.blue, label="Default Button", custom_id="button")])
