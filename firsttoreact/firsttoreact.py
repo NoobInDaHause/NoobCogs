97266d5fb227d1aedb0e2cf7c074520f9bc9eaf9
Winters <90045264+NoobInDaHause@users.noreply.github.com>
2023-03-09 02:11:39 +0800

    [firsttoreact] update 0.1.0

--- firsttoreact/firsttoreact.py
+++ firsttoreact/firsttoreact.py
@@ -11,7 +11,7 @@ class FirstToReact(commands.Cog):
     
     Cog suggested by: Cool aid man#3600
     
-    Cog version: 0.0.3
+    Cog version: 0.0.2
     Cog author: Noobindahause#2808
     """
     
@@ -20,30 +20,6 @@ class FirstToReact(commands.Cog):
         
     async def red_delete_data_for_user(self, **kwargs):
         return
-    
-    @commands.Cog.listener()
-    async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent):
-        channel = self.bot.get_channel(payload.channel_id)
-        message = await channel.fetch_message(payload.message_id)
-        user = self.bot.get_user(payload.user_id)
-        
-        if not user:
-            user = await self.bot.fetch_user(payload.user_id)
-        
-        if user.bot:
-            return
-        
-        if str(payload.emoji) == '⚡':
-            await message.clear_reactions()
-            await asyncio.sleep(2)
-            winner = discord.Embed(
-                title="\U0001F389 First To React Game Ended \U0001F389",
-                description=f"This first to react game has ended.\nCongratulations to {user.mention}! They are the first one to react!",
-                colour=0x2F3136,
-                timestamp=datetime.datetime.utcnow()
-            ).set_footer(text="Ended at")
-            await message.edit(content=user.mention, embed=winner)
-            await message.reply(f"{user.mention} has reacted first!")
     
     @commands.command(name="firsttoreact", aliases=["ftr"])
     @commands.guild_only()
@@ -56,13 +32,47 @@ class FirstToReact(commands.Cog):
         A very fun cog.
         """
             
-        emote = '⚡'
+        emote = '✅'
+        
+        em2 = discord.Embed(description="Please wait while I set up the game...")
+        msg = await ctx.send(embed=em2)
+        await asyncio.sleep(1)
+        
+        em3 = discord.Embed(description="Setup completed game starting in 10 seconds get ready.")
+        await msg.edit(embed=em3)
+        await asyncio.sleep(10)
         
         embed = discord.Embed(
-            title="\U0001F389 First To React Game \U0001F389",
-            description="First one to react with ⚡ wins the game!",
+            title=":tada: First To React Game Started :tada:",
+            description="After the game ends I will send the list of people who reacted.\nAnd to whoever is the first one on that list that means they are the winner and the first one to react.",
             colour=await ctx.embed_colour()
         ).set_footer(text=f"Hosted by: {ctx.author}", icon_url=ctx.author.avatar_url)
         
-        ftrmsg = await ctx.send(embed=embed)
-        await ftrmsg.add_reaction(emote)
+        await msg.edit(embed=embed)
+        await msg.add_reaction(emote)
+        await asyncio.sleep(10)
+        
+        message = await ctx.fetch_message(msg.id)
+        
+        players = []
+        
+        for reaction in message.reactions:
+            if reaction.emoji == '✅':
+                async for user in reaction.users():
+                    if user != self.bot.user:
+                        players.append(f"{user.mention}")
+                        await message.clear_reactions()
+        
+        if not players:
+            await ctx.send("Time is up and it seems no one has entered the game.")
+            return await msg.delete()
+        
+        endem = discord.Embed(
+            title=":tada: First To React Game Ended :tada:",
+            description="This first react game has ended.\nCongratulations to the first person on the list who reacted first.",
+            color=0x2F3136,
+            timestamp=datetime.datetime.utcnow()
+        ).set_footer(text="Ended at")
+        await message.edit(embed=endem)
+        
+        await ctx.send('\n'.join(players))
