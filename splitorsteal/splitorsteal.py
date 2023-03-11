226aed4bb3bf231d0451bcdd15338b0363909191
Winters <90045264+NoobInDaHause@users.noreply.github.com>
2023-03-11 13:13:44 +0800

    [splitorsteal] a couple of changes again

--- splitorsteal/splitorsteal.py
+++ splitorsteal/splitorsteal.py
@@ -49,9 +49,12 @@ class SplitOrSteal(commands.Cog):
         if prize == None:
             return await ctx.send("The game won't start without a prize.")
         
-        author = ctx.author
+        host = ctx.author
         user1 = player_1
         user2 = player_2
+        splitans = ["split", "🤝"]
+        stealans = ["steal", "⚔️"]
+        bothans = ["split", "🤝", "steal", "⚔️"]
         
         setupembed = discord.Embed(
             description = "Setting up game please wait..."
@@ -73,7 +76,7 @@ class SplitOrSteal(commands.Cog):
         )
         sotembed.add_field(name="Prize:", value=prize, inline=False)
         sotembed.set_footer(text="Be warned trust no one in this game. ;)", icon_url="https://static.vecteezy.com/system/resources/previews/012/042/301/original/warning-sign-icon-transparent-background-free-png.png")
-        sotembed.set_author(name=f"Hosted by: {author}", icon_url=author.avatar_url)
+        sotembed.set_author(name=f"Hosted by: {host}", icon_url=host.avatar_url)
         sotam = discord.AllowedMentions(roles=False, users=True, everyone=False)
         await ctx.send(content=f"{user1.mention} and {user2.mention}", embed=sotembed, allowed_mentions=sotam)
         await asyncio.sleep(60)
@@ -95,8 +98,9 @@ class SplitOrSteal(commands.Cog):
             
             dm1embed = discord.Embed(
                 colour = await ctx.embed_colour(),
-                description = "You may now choose. Do you want to `split` 🤝 or `steal` ⚔️?\nYou have 30 seconds to answer."
+                description = "You may now choose. Do you want to `split` 🤝 or `steal` ⚔️?"
             )
+            dm1embed.set_footer(text="You have 30 seconds to answer.")
             
             try:
                 await user1.send(embed=dm1embed)
@@ -107,39 +111,39 @@ class SplitOrSteal(commands.Cog):
             
             confirm = await ctx.bot.wait_for("message", check=check, timeout=30)
             
-            if confirm.content.lower() in ("split", "🤝"):
+            if confirm.content.lower() in splitans:
                 await user1.send(
                     "You have chosen split 🤝."
                 )
                 answer1 = "split"
                 
-            elif confirm.content.lower() in ("steal", "⚔️"):
+            elif confirm.content.lower() in stealans:
                 await user1.send(
                     "You have chosen steal ⚔️."
                 )
                 answer1 = "steal"
             
             else:
-                if confirm.content.lower() not in ("split", "steal", "🤝", "⚔️"):
+                if confirm.content.lower() not in bothans:
                     await user1.send(
                         "That is not a valid answer, answer `split` or `steal` or you will forfeit the game."
                     )
                     
                     confirm = await ctx.bot.wait_for("message", check=check, timeout=30)
                     
-                    if confirm.content.lower() in ("split", "🤝"):
+                    if confirm.content.lower() in splitans:
                         await user1.send(
                             "You have chosen split 🤝."
                         )
                         answer1 = "split"
                         
-                    if confirm.content.lower() in ("steal", "⚔️"):
+                    if confirm.content.lower() in stealans:
                         await user1.send(
                             "You have chosen steal ⚔️."
                         )
                         answer1 = "steal"
                         
-                    if confirm.content.lower() not in ("split", "steal", "🤝", "⚔️"):
+                    if confirm.content.lower() not in bothans:
                         await user1.send(
                             "You have failed to answer 2 times therefor you ferfeit the game."
                         )
@@ -153,9 +157,9 @@ class SplitOrSteal(commands.Cog):
                             description = f"This Split or Steal game has ended.\n{user1.mention} failed to answer `split` or `steal`!\n{user2.mention} chose nothing!"
                         )
                         failembed.add_field(name="Result:", value=failar, inline=False)
-                        failembed.set_footer(text=f"Thanks for playing! | Hosted by: {author}", icon_url=author.avatar_url)
+                        failembed.set_footer(text=f"Thanks for playing! | Hosted by: {host}", icon_url=host.avatar_url)
 
-                        await ctx.send(content=author.mention, embed=failembed)
+                        await ctx.send(content=host.mention, embed=failembed)
                         return
         except asyncio.TimeoutError:
             await user1.send(
@@ -171,9 +175,9 @@ class SplitOrSteal(commands.Cog):
                 description = f"This Split or Steal game has ended.\n{user1.mention} took too long to answer!\n{user2.mention} chose nothing!"
             )
             failembed.add_field(name="Result:", value=failar, inline=False)
-            failembed.set_footer(text=f"Thanks for playing! | Hosted by: {author}", icon_url=author.avatar_url)
+            failembed.set_footer(text=f"Thanks for playing! | Hosted by: {host}", icon_url=host.avatar_url)
                         
-            await ctx.send(content=author.mention, embed=failembed)
+            await ctx.send(content=host.mention, embed=failembed)
             return
         
         try:
@@ -181,9 +185,10 @@ class SplitOrSteal(commands.Cog):
                 return m.author == user2 and m.channel == user2.dm_channel
             
             dm2embed = discord.Embed(
-                description = "You may now choose. Do you want to `split` 🤝 or `steal` ⚔️?\nYou have 30 seconds to answer.",
+                description = "You may now choose. Do you want to `split` 🤝 or `steal` ⚔️?",
                 colour = await ctx.embed_colour()
             )
+            dm2embed.set_footer(text="You have 30 seconds to answer.")
             
             try:
                 await user2.send(embed=dm2embed)
@@ -194,39 +199,39 @@ class SplitOrSteal(commands.Cog):
             
             confirm = await ctx.bot.wait_for("message", check=check, timeout=30)
             
-            if confirm.content.lower() in ("split", "🤝"):
+            if confirm.content.lower() in splitans:
                 await user2.send(
                     "You have chosen split 🤝."
                 )
                 answer2 = "split"
             
-            elif confirm.content.lower() in ("steal", "⚔️"):
+            elif confirm.content.lower() in stealans:
                 await user2.send(
                     "You have chosen steal ⚔️."
                 )
                 answer2 = "steal"
             
             else:
-                if confirm.content.lower() not in ("split", "steal", "🤝", "⚔️"):
+                if confirm.content.lower() not in bothans:
                     await user2.send(
                         "That is not a valid answer, answer `split` or `steal` or you will forfeit the game."
                     )
                     
                     confirm = await ctx.bot.wait_for("message", check=check, timeout=30)
                     
-                    if confirm.content.lower() in ("split", "🤝"):
+                    if confirm.content.lower() in splitans:
                         await user2.send(
                             "You have chosen split 🤝."
                         )
                         answer2 = "split"
                         
-                    if confirm.content.lower() in ("steal", "⚔️"):
+                    if confirm.content.lower() in stealans:
                         await user2.send(
                             "You have chosen steal ⚔️."
                         )
                         answer2 = "steal"
                         
-                    if confirm.content.lower() not in ("split", "steal", "🤝", "⚔️"):
+                    if confirm.content.lower() not in bothans:
                         await user2.send(
                             "You have failed to answer 2 times therefor you ferfeit the game."
                         )
@@ -240,9 +245,9 @@ class SplitOrSteal(commands.Cog):
                             description = f"This Split or Steal game has ended.\n{user1.mention} chose {answer1.capitalize()}!\n{user2.mention} failed to answer `split` or `steal`!"
                         )
                         failembed.add_field(name="Result:", value=failar, inline=False)
-                        failembed.set_footer(text=f"Thanks for playing! | Hosted by: {author}", icon_url=author.avatar_url)
+                        failembed.set_footer(text=f"Thanks for playing! | Hosted by: {host}", icon_url=host.avatar_url)
                         
-                        await ctx.send(content=author.mention, embed=failembed)
+                        await ctx.send(content=host.mention, embed=failembed)
                         return
         except asyncio.TimeoutError:
             await user2.send(
@@ -258,9 +263,9 @@ class SplitOrSteal(commands.Cog):
                 description = f"This Split or Steal game has ended.\n{user1.mention} chose {answer1.capitalize()}!\n{user2.mention} took too long to answer!"
             )
             failembed.add_field(name="Result:", value=failar, inline=False)
-            failembed.set_footer(text=f"Thanks for playing! | Hosted by: {author}", icon_url=author.avatar_url)
+            failembed.set_footer(text=f"Thanks for playing! | Hosted by: {host}", icon_url=host.avatar_url)
                         
-            await ctx.send(content=author.mention, embed=failembed)
+            await ctx.send(content=host.mention, embed=failembed)
             return
         
         if answer1 and answer2 == "split":
@@ -286,6 +291,6 @@ class SplitOrSteal(commands.Cog):
             description = f"This Split or Steal game has ended.\n{user1.mention} chose {answer1.capitalize()}!\n{user2.mention} chose {answer2.capitalize()}!"
         )
         gameoverembed.add_field(name="Result:", value=result, inline=False)
-        gameoverembed.set_footer(text=f"Thanks for playing! | Hosted by: {author}", icon_url=author.avatar_url)
+        gameoverembed.set_footer(text=f"Thanks for playing! | Hosted by: {host}", icon_url=host.avatar_url)
         
-        await ctx.send(content=author.mention, embed=gameoverembed)
+        await ctx.send(content=host.mention, embed=gameoverembed)
