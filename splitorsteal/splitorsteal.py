ba0a35c228a9068b49988770a2cd1c7496a250bd
Winters <90045264+NoobInDaHause@users.noreply.github.com>
2023-03-13 10:41:53 +0800

    [splitorsteal] style reformatting and couple of changes

--- splitorsteal/splitorsteal.py
+++ splitorsteal/splitorsteal.py
@@ -1,10 +1,11 @@
-import discord
-import logging
 import asyncio
 import datetime
+import discord
+import logging
 
 from redbot.core import commands
 from redbot.core.bot import Red
+
 from disputils import BotEmbedPaginator
 
 log = logging.getLogger("red.winterscogs.splitorsteal")
@@ -12,12 +13,14 @@ log = logging.getLogger("red.winterscogs.splitorsteal")
 class SplitOrSteal(commands.Cog):
     """
     A fun split or steal game.
+
+    This game can shatter friendships.
     """
     
     def __init__(self, bot: Red) -> None:
         self.bot = bot
         
-    __version__ = "1.4.2"
+    __version__ = "1.4.3"
     __author__ = ["Noobindahause#2808"]
     
     def format_help_for_context(self, ctx: commands.Context) -> str:
@@ -34,16 +37,23 @@ class SplitOrSteal(commands.Cog):
     @commands.bot_has_permissions(embed_links=True)
     async def sosrules(self, ctx):
         """
+        Some useful information about split or steal.
+
         Know how to play or what the rules of split or steal game is.
         """
         emotes = ["⏪", "◀️", "▶️", "⏩", "❌"]
 
         em1desc = """
         The game involves a segment called `Split or Steal` in which two participants or players make the decision to either `split` or `steal` and to determine how the prize is divided or stolen.
+        
         The game also determines the ones who are good or evil.
+        
         The game requires 2 participants or players to play.
+        
         A very fun game to play.
+        
         :warning: This game can shatter friendships! Play at your own risk!
+        
         Happy playing! :)
         """
 
@@ -82,14 +92,14 @@ class SplitOrSteal(commands.Cog):
         em1.set_footer(text=f"Command executed by: {ctx.author} | Page", icon_url=ctx.author.avatar_url)
 
         em2 = discord.Embed(
-            title = "***__Rules__***",
+            title = "***__Rules of Split or Seal__***",
             description = em2desc,
             colour = await ctx.embed_colour()
         )
         em2.set_footer(text=f"Command executed by: {ctx.author} | Page", icon_url=ctx.author.avatar_url)
 
         em3 = discord.Embed(
-            title = "***__How the game works__***",
+            title = "***__How Split or Steal works__***",
             description = em3desc,
             colour = await ctx.embed_colour()
         )
@@ -103,14 +113,18 @@ class SplitOrSteal(commands.Cog):
     @commands.guild_only()
     @commands.cooldown(1, 10, commands.BucketType.user)
     @commands.bot_has_permissions(embed_links=True)
-    async def splitorsteal(self, ctx, player_1: discord.Member = None, player_2: discord.Member = None, *, prize = None):
+    async def splitorsteal(
+        self, ctx, player_1: discord.Member = None, player_2: discord.Member = None, *, prize = None
+    ):
         """
         Start a split or steal game event.
+
+        Fun game to play.
         """
         
-        if player_1 == None:
+        if not player_1:
             return await ctx.send("This game requires 2 users to play.")
-        if player_2 == None:
+        if not player_2:
             return await ctx.send("This game requires 2 users to play.")
         if player_1 == player_2:
             return await ctx.send("This game requires 2 users to play.")
@@ -141,9 +155,11 @@ class SplitOrSteal(commands.Cog):
         await asyncio.sleep(3)
         await setup.delete()
         
+        sotdesc = "The split or steal game has begun!\nPlayers now have 1 minute to discuss if they want to either split 🤝 or steal ⚔️.\nThink very carefully and make your decisions precise!"
+        
         sotembed = discord.Embed(
             title = "Split or Steal Game",
-            description = "The split or steal game has begun!\nPlayers now have 1 minute to discuss if they want to either split 🤝 or steal ⚔️.\nThink very carefully and make your decisions precise!",
+            description = sotdesc,
             colour = await ctx.embed_colour()
         )
         sotembed.add_field(name="Prize:", value=prize, inline=False)
@@ -153,7 +169,9 @@ class SplitOrSteal(commands.Cog):
         await ctx.send(content=f"{user1.mention} and {user2.mention}", embed=sotembed, allowed_mentions=sotam)
         await asyncio.sleep(60)
         
-        await ctx.send(f"Time is up! I will now DM the players if they choose to split 🤝 or steal ⚔️.\n> {user1.mention} and {user2.mention} make sure you have your DM's open for me to send message.")
+        await ctx.send(
+            f"Time is up! I will now DM the players if they choose to split 🤝 or steal ⚔️.\n> {user1.mention} and {user2.mention} make sure you have your DM's open for me to send message."
+        )
         await asyncio.sleep(3)
         
         try:
@@ -233,8 +251,7 @@ class SplitOrSteal(commands.Cog):
                         failembed.add_field(name="Result:", value=failar, inline=False)
                         failembed.set_footer(text=f"Thanks for playing! | Hosted by: {host}", icon_url=host.avatar_url)
 
-                        await ctx.send(content=host.mention, embed=failembed)
-                        return
+                        return await ctx.send(content=host.mention, embed=failembed)
         except asyncio.TimeoutError:
             await user1.send(
                 "You took too long to respond."
@@ -251,8 +268,7 @@ class SplitOrSteal(commands.Cog):
             failembed.add_field(name="Result:", value=failar, inline=False)
             failembed.set_footer(text=f"Thanks for playing! | Hosted by: {host}", icon_url=host.avatar_url)
                         
-            await ctx.send(content=host.mention, embed=failembed)
-            return
+            return await ctx.send(content=host.mention, embed=failembed)
         
         try:
             def check(m):
@@ -322,8 +338,7 @@ class SplitOrSteal(commands.Cog):
                         failembed.add_field(name="Result:", value=failar, inline=False)
                         failembed.set_footer(text=f"Thanks for playing! | Hosted by: {host}", icon_url=host.avatar_url)
                         
-                        await ctx.send(content=host.mention, embed=failembed)
-                        return
+                        return await ctx.send(content=host.mention, embed=failembed)
         except asyncio.TimeoutError:
             await user2.send(
                 "You took too long to respond."
@@ -340,8 +355,7 @@ class SplitOrSteal(commands.Cog):
             failembed.add_field(name="Result:", value=failar, inline=False)
             failembed.set_footer(text=f"Thanks for playing! | Hosted by: {host}", icon_url=host.avatar_url)
                         
-            await ctx.send(content=host.mention, embed=failembed)
-            return
+            return await ctx.send(content=host.mention, embed=failembed)
         
         if answer1 and answer2 == "split":
             result = f"Both players chose split 🤝! They can now split the **{prize}** prize."
@@ -359,11 +373,13 @@ class SplitOrSteal(commands.Cog):
             result = f"{user1.mention} steals the **{prize}** prize from {user2.mention} ⚔️!"
             col = 0xFF0000
         
+        gameoverdesc = f"This Split or Steal game has ended.\n{user1.mention} chose {answer1.capitalize()}!\n{user2.mention} chose {answer2.capitalize()}!"
+        
         gameoverembed = discord.Embed(
             colour = col,
             timestamp = datetime.datetime.utcnow(),
             title = "Game over",
-            description = f"This Split or Steal game has ended.\n{user1.mention} chose {answer1.capitalize()}!\n{user2.mention} chose {answer2.capitalize()}!"
+            description = gameoverdesc,
         )
         gameoverembed.add_field(name="Result:", value=result, inline=False)
         gameoverembed.set_footer(text=f"Thanks for playing! | Hosted by: {host}", icon_url=host.avatar_url)
