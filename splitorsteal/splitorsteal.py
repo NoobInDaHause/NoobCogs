6d4c644023a24c42a9c1ab3d0ade9bbf8a514741
Winters <90045264+NoobInDaHause@users.noreply.github.com>
2023-03-19 17:28:22 +0800

    forgot version again and again and again and again and again

--- splitorsteal/splitorsteal.py
+++ splitorsteal/splitorsteal.py
@@ -33,7 +33,7 @@ class SplitOrSteal(commands.Cog):
         self.config.register_guild(**default_guild_settings)
         self.log = logging.getLogger("red.WintersCogs.splitorsteal")
         
-    __version__ = "2.1.9"
+    __version__ = "2.1.10"
     __author__ = ["Noobindahause#2808"]
     
     def format_help_for_context(self, ctx: commands.Context) -> str:
