482fb5270b27675bb305178549c977ab814d5f5a
Winters <90045264+NoobInDaHause@users.noreply.github.com>
2023-04-16 17:49:06 +0800

    Update buttons.py

--- afk/buttons.py
+++ afk/buttons.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import Dict, Optional, Union, List, Any, Union
+from typing import Dict, Optional, Union, List, Any
 
 import discord
 from discord.ext import commands
