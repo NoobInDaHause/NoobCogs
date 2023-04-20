206a613ec6182f7338093a901527ffde78669bfa
Winters <90045264+NoobInDaHause@users.noreply.github.com>
2023-04-20 09:51:51 +0800

    [all] update eud again

--- afk/__init__.py
+++ afk/__init__.py
@@ -1,15 +1,7 @@
-import json
-
-from pathlib import Path
 from redbot.core.bot import Red
+from redbot.core.utils import get_end_user_data_statement
 
-with open(Path(__file__).parent / "info.json") as fp:
-    """
-    This cog stores data provided by users for the express purpose of notifying users whenever they go AFK and only for that reason. It does not store user data which was not provided through a command. Users may remove their own content without making a data removal request. This cog does not support data requests, but will respect deletion requests.
-    
-    Thanks aikaterna and some others for this end user data statement.
-    """
-    __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
+__red_end_user_data_statement__ = get_end_user_data_statement(__file__)
     
 from .afk import Afk
 
