b5e37818dd615d9c8bce982afb036857867bc423
Winters <90045264+NoobInDaHause@users.noreply.github.com>
2023-04-28 14:12:49 +0800

    hmm

--- rainbowrole/rainbowrole.py
+++ rainbowrole/rainbowrole.py
@@ -57,7 +57,7 @@ class RainbowRole(commands.Cog):
     
     async def cog_unload(self):
         s = self.change_rainbowrole_color.stop()
-        self.bot.loop.create_task(s)
+        await self.bot.loop.create_task(s)
         self.log.info("Rainbowrole task cancelled.")
     
     @tasks.loop(minutes=10)
