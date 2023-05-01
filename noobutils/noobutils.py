68b8e3dd4fc82df4d5a6c5b27d9e50f34360d044
Noob <90045264+NoobInDaHause@users.noreply.github.com>
2023-05-02 00:03:15 +0800

    E

--- noobutils/noobutils.py
+++ noobutils/noobutils.py
@@ -116,7 +116,7 @@ class NoobUtils(commands.Cog):
       
     @testlog.error
     async def testlog_error(self, context: commands.Context, error):
-        if isinstance(error, commands.MissingPermissions):
+        if isinstance(error, app_commands.CheckFailure):
             return await context.reply(content=self.access_denied(), ephemeral=True, mention_author=False)
 
         if isinstance(error, commands.MissingRequiredArgument):
