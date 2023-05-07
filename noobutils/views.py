097886c78abe7e52d6104dc8601085f6f10d0469
Winters <90045264+NoobInDaHause@users.noreply.github.com>
2023-05-07 12:28:09 +0800

    AUGHHHHHHHHHHHHHH

--- noobutils/views.py
+++ noobutils/views.py
@@ -232,7 +232,7 @@ class PressFButton(discord.ui.Button):
     
     async def callback(self, interaction: discord.Interaction):
         self.view.paid_users.append(interaction.user.id)
-        self.label = len(self.view.paid_users + 1)
+        self.label = len(self.view.paid_users) + 1
         await interaction.response.send_message(content=f"**{interaction.user.name}** has paid their respects.")
 
 class PressFView(discord.ui.View):
