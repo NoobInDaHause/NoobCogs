a9667d3d6c44f65d1d8482f4a036b7df7e78b2e6
Noob <90045264+NoobInDaHause@users.noreply.github.com>
2023-05-12 23:13:51 +0800

    a

--- reach/reach.py
+++ reach/reach.py
@@ -113,6 +113,7 @@ class Reach(commands.Cog):
         final = []
         for i in input_roles:
             try:
+                i.replace("<", "").replace("@", "")replace("&", "")replace(">", "")
                 j = context.guild.get_role(int(i))
                 conf_roles.append(j)
             except Exception:
