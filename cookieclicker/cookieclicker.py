3ea02e3b91417b03444b0a624f2fe6e5c848e725
Winters <90045264+NoobInDaHause@users.noreply.github.com>
2023-10-25 00:53:31 +0800

    forgetme fix #2

--- cookieclicker/cookieclicker.py
+++ cookieclicker/cookieclicker.py
@@ -26,7 +26,7 @@ class CookieClicker(commands.Cog):
         self.config.register_guild(**default_guild)
         self.log = logging.getLogger("red.NoobCogs.PressF")
 
-    __version__ = "1.1.7"
+    __version__ = "1.1.8"
     __author__ = ["NoobInDaHause"]
     __docs__ = (
         "https://github.com/NoobInDaHause/NoobCogs/blob/red-3.5/cookieclicker/README.md"
@@ -144,7 +144,7 @@ class CookieClicker(commands.Cog):
 
         if view.value:
             async with self.config.guild(context.guild).user_lb() as ulb:
-                del ulb[context.author.id]
+                del ulb[str(context.author.id)]
 
     @cookieclickerset.command(name="emoji")
     @commands.admin_or_permissions(manage_guild=True)
