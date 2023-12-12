dc07679a155a557fae0e24a1873e789dd4a98e8e
Winters <90045264+NoobInDaHause@users.noreply.github.com>
2023-12-12 10:37:27 +0800

    highlight command invoker

--- donationlogger/donationlogger.py
+++ donationlogger/donationlogger.py
@@ -45,7 +45,7 @@ class DonationLogger(commands.Cog):
         self.log = logging.getLogger("red.NoobCogs.DonationLogger")
         self.setupcache = []
 
-    __version__ = "1.0.17"
+    __version__ = "1.0.18"
     __author__ = ["NoobInDaHause"]
     __docs__ = "https://github.com/NoobInDaHause/NoobCogs/blob/red-3.5/donationlogger/README.md"
 
@@ -94,7 +94,9 @@ class DonationLogger(commands.Cog):
         for k, v in sorted_donators:
             member = context.guild.get_member(int(k))
             m = (
-                f"{member.mention} (`{member.id}`): **{cf.humanize_number(v)}**"
+                f"➡️ {member.mention} (`{member.id}`): **{cf.humanize_number(v)}**"
+                if member and member == context.author
+                else f"{member.mention} (`{member.id}`): **{cf.humanize_number(v)}**"
                 if member
                 else f"[Member not found in guild] (`{k}`): **{cf.humanize_number(v)}**"
             )
@@ -474,7 +476,9 @@ class DonationLogger(commands.Cog):
         ):
             member = context.guild.get_member(int(donator_id))
             mention = (
-                f"{member.mention} (`{member.id}`)"
+                f"➡️ {member.mention} (`{member.id}`)"
+                if member and member == context.author
+                else f"{member.mention} (`{member.id}`)"
                 if member
                 else f"Member not found in server. (`{donator_id}`)"
             )
