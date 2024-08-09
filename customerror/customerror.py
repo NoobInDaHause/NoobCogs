57f451f836e6d11f4a629151f4010a379d948b7a
Noob <90045264+NoobInDaHause@users.noreply.github.com>
2024-08-09 10:53:03 +0800

    applied fixes

--- customerror/customerror.py
+++ customerror/customerror.py
@@ -29,7 +29,7 @@ class CustomError(nu.Cog):
         super().__init__(
             bot=bot,
             cog_name=self.__class__.__name__,
-            version="1.2.3",
+            version="1.2.4",
             authors=["NoobInDaHause"],
             use_config=True,
             identifier=9874825374237,
@@ -92,7 +92,7 @@ class CustomError(nu.Cog):
                 "message_id": tse.StringAdapter(context.message.id),
                 "message_jump_url": tse.StringAdapter(context.message.jump_url),
             }
-            if g := (getattr(context.author, "guild", None) or context.guild:
+            if g := (getattr(context.author, "guild", None) or context.guild):
                 to_convert["guild"] = tse.GuildAdapter(g)
             msg = await self.config.error_msg()
             processed = await tagengine.process(message=msg, seed_variables=to_convert)
