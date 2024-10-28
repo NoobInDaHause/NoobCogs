e45f2eb32a2243d21e99ef061ee65ce227ffa52f
Winters <90045264+NoobInDaHause@users.noreply.github.com>
2024-10-28 19:20:23 +0800

    ...

--- automaticreaction/automaticreaction.py
+++ automaticreaction/automaticreaction.py
@@ -21,7 +21,7 @@ class AutomaticReaction(nu.Cog):
         super().__init__(
             bot=bot,
             cog_name=self.__class__.__name__,
-            version="1.0.7",
+            version="1.0.8",
             authors=["NoobInDaHause"],
             use_config=True,
             force_registration=True,
